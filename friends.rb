a474079fbb9f71fe47f37d0d8e6b411380a75c43
Dylan Timmons <dylanstimmons@icloud.com>
2023-04-12 15:22:45 -0600

    add arrays and an array method

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Allen", "Joe", "Tina", "Bob", "Ken"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["Cairo", "Moab", "Denver", "Vail", "Aspen"]
+puts places.pop()
+puts places
